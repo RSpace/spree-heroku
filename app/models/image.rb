5ab6c075c5d593313235da996123aaee0bd8b4ae
Pavel Chipiga <pavel.chipiga@gmail.com>
2010-06-02 19:13:39 +0300

    Fix s3 path/url template at Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,8 +3,7 @@ class Image < Asset
   has_attached_file :attachment, 
                     :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' }, 
                     :default_style => :product,
-                    :url => "/assets/products/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension",
+                    :path => "assets/products/:id/:style/:basename.:extension",
                     :storage => "s3",
                     :s3_credentials => {
                       :access_key_id => HEROKU_AWS_S3['access_key_id'],
