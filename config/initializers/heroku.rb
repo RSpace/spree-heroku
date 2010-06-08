a770c3e07a73f6b4d19990905d6984eed51cb595
Casper Fabricius <me@casperfabricius.com>
2010-06-08 21:30:58 +0200

    Updated initializer to work without S3 YAML file

--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
-begin
-  HEROKU_AWS_S3 = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'aws_s3.yml'))[Rails.env]
-rescue
-  HEROKU_AWS_S3 = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'aws_s3.yml.example'))[Rails.env]
+aws_s3_file = File.join(File.dirname(__FILE__), '..', 'aws_s3.yml')
+
+if File.exists?(aws_s3_file)
+  HEROKU_AWS_S3 = YAML.load_file(aws_s3_file)[Rails.env]
+else
+  HEROKU_AWS_S3 = {}
 end
